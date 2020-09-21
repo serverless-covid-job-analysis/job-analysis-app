e1423991e081c210465122e59f0687bb4629565f
shubhindia <shubhindia123@gmail.com>
2020-09-21 16:09:29 +0530

    use generted data
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,77 +7,81 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
-
   Future<List<TrainJourneySeries>> fetchAlbum() async {
-  final response =
-      await http.get('https://jsonplaceholder.typicode.com/albums/1');
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    // var temp = json.decode(response.body);
-    var temp = [
-   {
-      "index":0,
-      "source_state":"MAH",
-      "source_city":"Mumbai",
-      "destination_state":"Bihar",
-      "destination_city":"Bhagalpur",
-      "number_of_trains":15,
-      "number_of_passengers":176859
-   },
-   {
-      "index":1,
-      "source_state":"UP",
-      "source_city":"Gopalpur",
-      "destination_state":"Bihar",
-      "destination_city":"Bhagalpur",
-      "number_of_trains":15,
-      "number_of_passengers":136852
-   },
-   {
-      "index":2,
-      "source_state":"MP",
-      "source_city":"Indore",
-      "destination_state":"Bihar",
-      "destination_city":"Bhagalpur",
-      "number_of_trains":15,
-      "number_of_passengers":116857
-   },
-   {
-      "index":3,
-      "source_state":"WB",
-      "source_city":"Gopalpur",
-      "destination_state":"Bihar",
-      "destination_city":"Bhagalpur",
-      "number_of_trains":15,
-      "number_of_passengers":156857
-   },
-   {
-      "index":4,
-      "source_state":"KL",
-      "source_city":"Gopalpur",
-      "destination_state":"Bihar",
-      "destination_city":"Bhagalpur",
-      "number_of_trains":15,
-      "number_of_passengers":186851
-   }
-];
+    final response =
+        await http.get('https://api.jsonbin.io/b/5f6880407243cd7e8240921b/3');
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      var temp = json.decode(response.body);
+      print(temp);
+      /*var temp = [
+        {
+          "index": 0,
+          "source_state": "MAH",
+          "source_city": "Mumbai",
+          "destination_state": "Bihar",
+          "destination_city": "Bhagalpur",
+          "number_of_trains": 15,
+          "number_of_passengers": 176859
+        },
+        {
+          "index": 1,
+          "source_state": "UP",
+          "source_city": "Gopalpur",
+          "destination_state": "Bihar",
+          "destination_city": "Bhagalpur",
+          "number_of_trains": 15,
+          "number_of_passengers": 136852
+        },
+        {
+          "index": 2,
+          "source_state": "MP",
+          "source_city": "Indore",
+          "destination_state": "Bihar",
+          "destination_city": "Bhagalpur",
+          "number_of_trains": 15,
+          "number_of_passengers": 116857
+        },
+        {
+          "index": 3,
+          "source_state": "WB",
+          "source_city": "Gopalpur",
+          "destination_state": "Bihar",
+          "destination_city": "Bhagalpur",
+          "number_of_trains": 15,
+          "number_of_passengers": 156857
+        },
+        {
+          "index": 4,
+          "source_state": "KL",
+          "source_city": "Gopalpur",
+          "destination_state": "Bihar",
+          "destination_city": "Bhagalpur",
+          "number_of_trains": 15,
+          "number_of_passengers": 186851
+        }
+      ];*/
 
-    print(temp.length);
-    List<TrainJourneySeries> data = [];
-    for (var t in temp) {
-        TrainJourneySeries train = TrainJourneySeries(t['source_state'],t['number_of_passengers'],charts.ColorUtil.fromDartColor(Colors.blue));
+      print(temp.length);
+      List<TrainJourneySeries> data = [];
+      for (var t in temp) {
+        TrainJourneySeries train = TrainJourneySeries(
+            t['source_city'],
+            t['number_of_outgoing_passengers'],
+            charts.ColorUtil.fromDartColor(Colors.blue));
         data.add(train);
+      }
+      return data;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
     }
-    return data;
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
   }
-}
-    final List<TrainJourneySeries> data = [];
-  
+
+  final List<TrainJourneySeries> data = [];
+
   @override
   Widget build(BuildContext context) {
     print('hi');
@@ -87,23 +91,19 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.red,
         title: Text("Outward Traffic from States"),
       ),
-        body: Container(
-            child: FutureBuilder(
-                future: fetchAlbum(),
-                builder: (BuildContext context, AsyncSnapshot snapshot){
-                    if (snapshot.data == null) {
-                      return Container(
-                        child: Center(
-                          child: Text("Loading...")
-                        )
-                      );
-                    }
-                    return TrainJourneyChart(
-                        data: snapshot.data,
-                    );
-                },
-            ),
+      body: Container(
+        child: FutureBuilder(
+          future: fetchAlbum(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Container(child: Center(child: Text("Loading...")));
+            }
+            return TrainJourneyChart(
+              data: snapshot.data,
+            );
+          },
         ),
+      ),
     );
   }
 }
